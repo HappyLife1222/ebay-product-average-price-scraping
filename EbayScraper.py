d8b6531cfb861fdc114606dfc936d2a0923b3c9c
Viktor Pavlenko <pavlenkoviktor44@outlook.com>
2024-04-04 22:25:35 +0300

    first commint

---
+++ EbayScraper.py
@@ -0,0 +1,191 @@
+import re
+import urllib.parse
+import urllib.request
+from bs4 import BeautifulSoup
+
+countryDict = {
+    'au': '.com.au',
+    'at': '.at',
+    'be': '.be',
+    'ca': '.ca',
+    'ch': '.ch',
+    'de': '.de',
+    'es': '.es',
+    'fr': '.fr',
+    'hk': '.com.hk',
+    'ie': '.ie',
+    'it': '.it',
+    'my': '.com.my',
+    'nl': '.nl',
+    'nz': '.co.nz',
+    'ph': '.ph',
+    'pl': '.pl',
+    'sg': '.com.sg',
+    'uk': '.co.uk',
+    'us': '.com',
+}
+
+conditionDict = {
+    'all': '',
+    'new': '&LH_ItemCondition=1000',
+    'opened': '&LH_ItemCondition=1500',
+    'refurbished': '&LH_ItemCondition=2500',
+    'used': '&LH_ItemCondition=3000'
+}
+
+typeDict = {
+    'all': '&LH_All=1',
+    'auction': '&LH_Auction=1',
+    'bin': '&LH_BIN=1',
+    'offers': '&LH_BO=1'
+}
+
+def Items(query, country='us', condition='all', type='all'):
+    
+    if country not in countryDict:
+        raise Exception('Country not supported, please use one of the following: ' + ', '.join(countryDict.keys()))
+
+    if condition not in conditionDict:
+        raise Exception('Condition not supported, please use one of the following: ' + ', '.join(conditionDict.keys()))
+        
+    if type not in typeDict:
+        raise Exception('Type not supported, please use one of the following: ' + ', '.join(typeDict.keys()))
+    
+    soup = __GetHTML(query, country, condition, type, alreadySold=False)
+    data = __ParseItems(soup)
+    
+    return data
+    
+def Average(query, country='us', condition='all'):
+    
+    if country not in countryDict:
+        raise Exception('Country not supported, please use one of the following: ' + ', '.join(countryDict.keys()))
+
+    if condition not in conditionDict:
+        raise Exception('Condition not supported, please use one of the following: ' + ', '.join(conditionDict.keys()))
+    
+    soup = __GetHTML(query, country, condition, type='all', alreadySold=True)
+    data = __ParsePrices(soup)
+    
+    avgPrice = round(__Average(data['price-list']), 2)
+    avgShipping = round(__Average(data['shipping-list']), 2)
+    
+    return {
+        'price': avgPrice,
+        'shipping': avgShipping,
+        'total': round(avgPrice + avgShipping, 2)
+    }
+
+def __GetHTML(query, country, condition='', type='all', alreadySold=True):
+    
+    alreadySoldString = '&LH_Complete=1&LH_Sold=1' if alreadySold else ''
+    
+    # Build the URL
+    parsedQuery = urllib.parse.quote(query).replace('%20', '+')
+    url = f'https://www.ebay{countryDict[country]}/sch/i.html?_from=R40&_nkw=' + parsedQuery + alreadySoldString + conditionDict[condition] + typeDict[type]
+
+    # Get the web page HTML
+    request = urllib.request.urlopen(url)
+    soup = BeautifulSoup(request.read(), 'html.parser')
+
+    return soup
+
+def __ParseItems(soup):
+    rawItems = soup.find_all('div', {'class': 's-item__info clearfix'})
+    data = []
+
+    for item in rawItems[1:]:
+        
+        #Get item data
+        title = item.find(class_="s-item__title").find('span').get_text(strip=True)
+        
+        price = __ParseRawPrice(item.find('span', {'class': 's-item__price'}).get_text(strip=True))
+        
+        try: shipping = __ParseRawPrice(item.find('span', {'class': 's-item__shipping s-item__logisticsCost'}).find('span', {'class': 'ITALIC'}).get_text(strip=True))
+        except: shipping = 0
+        
+        try: timeLeft = item.find(class_="s-item__time-left").get_text(strip=True)
+        except: timeLeft = ""
+        
+        try: timeEnd = item.find(class_="s-item__time-end").get_text(strip=True)
+        except: timeEnd = ""
+        
+        try: bidCount = int("".join(filter(str.isdigit, item.find(class_="s-item__bids s-item__bidCount").get_text(strip=True))))
+        except: bidCount = 0
+        
+        try: reviewCount = int("".join(filter(str.isdigit, item.find(class_="s-item__reviews-count").find('span').get_text(strip=True))))
+        except: reviewCount = 0
+        
+        url = item.find('a')['href']
+
+        itemData = {
+            'title': title,
+            'price': price,
+            'shipping': shipping,
+            'time-left': timeLeft,
+            'time-end': timeEnd,
+            'bid-count': bidCount,
+            'reviews-count': reviewCount,
+            'url': url
+        }
+        
+        data.append(itemData)
+    
+    # Remove item with prices too high or too low
+    priceList = [item['price'] for item in data]
+    parsedPriceList = __StDevParse(priceList)
+    data = [item for item in data if item['price'] in parsedPriceList]
+    
+    return sorted(data, key=lambda dic: dic['price'] + dic['shipping'])
+
+def __ParsePrices(soup):
+    
+    # Get item prices
+    rawPriceList = [price.get_text(strip=True) for price in soup.find_all(class_="s-item__price")]
+    priceList = [price for price in map(lambda rawPrice:__ParseRawPrice(rawPrice), rawPriceList) if price != None]
+    
+    # Get shipping prices
+    rawShippingList = [item.get_text(strip=True) for item in soup.find_all(class_="s-item__shipping s-item__logisticsCost")]
+    shippingList = map(lambda rawPrice:__ParseRawPrice(rawPrice), rawShippingList)
+    shippingList = [0 if price == None else price for price in shippingList]
+
+    # Remove prices too high or too low
+    priceList = __StDevParse(priceList)
+    shippingList = __StDevParse(shippingList)
+
+    data = {
+        'price-list': priceList,
+        'shipping-list': shippingList
+    }
+    return data
+
+def __ParseRawPrice(string):
+    parsedPrice = re.search('(\d+(.\d+)?)', string.replace(',', '.'))
+    if (parsedPrice):
+        return float(parsedPrice.group())
+    else:
+        return None
+
+def __Average(numberList):
+
+    if len(list(numberList)) == 0: return 0
+    return sum(numberList) / len(list(numberList))
+
+def __StDev(numberList):
+    
+    if len(list(numberList)) <= 1: return 0
+    
+    nominator = sum(map(lambda x: (x - sum(numberList) / len(numberList)) ** 2, numberList))
+    stdev = (nominator / ( len(numberList) - 1)) ** 0.5
+
+    return stdev
+
+def __StDevParse(numberList):
+    
+    avg = __Average(numberList)
+    stdev = __StDev(numberList)
+    
+    # Remove prices too high or too low; Accept Between -1 StDev to +1 StDev
+    numberList = [nmbr for nmbr in numberList if (avg + stdev >= nmbr >= avg - stdev)]
+
+    return numberList
---
+++ README.md
@@ -0,0 +1,49 @@
+# Ebay Scraper
+#### This script has 2 main functions:
+1. Get the average price of a product based on already sold ones.
+2. Get a list of products on sale.
+
+## Requirements: ##
+
+- **[BeautifulSoup4](https://pypi.org/project/beautifulsoup4/)**
+
+## How to Use: ##
+
+1. Add *EbayScraper.py* to your project directory.
+2. Import it.
+3. Your Done! You can now search for anything. *(See below for more details)*
+
+---
+
+### Average Function:
+
+Here we search for a new Nintendo switch on eBay Canada. The first parameter is for the *search query*, the second one is for the *country* and the third one is for the *condition* of the item.
+```PYTHON
+import EbayScraper
+
+averagePrice = EbayScraper.Average(query='Nintendo Switch', country='ca', condition='new')
+print(averagePrice)
+```
+#### Output:
+The output will be a **dictionary** with the average sold price, the average shipping price and the average total price of the item searched. Here we can see that the average price for a brand new Nintendo Switch on eBay, including the shipping, is **365.87 CAD**.
+```PYTHON
+{'price': 326.2, 'shipping': 39.67, 'total': 365.87}
+```
+
+---
+
+
+### Items Function:
+
+Like the **Average Function**, the first parameter is for the *search query*, the second one is for the *country* and the third one is for the *condition* of the item. The difference here is that this function return a **list** of **dictionary** with data about each item. Since the **list** returned is sorted by price + shipping, here we get the cheapest auction for an RTX 3060 in Canada.
+```PYTHON
+import EbayScraper
+
+itemList = EbayScraper.Items(query='RTX 3060', country='ca', condition='new', type='auction')
+print(itemList[0])
+```
+#### Output:
+The output will be a **dictionary** with all the information about the item.
+```PYTHON
+{'title': 'MSI Gaming GeForce RTX 3060 12GB NEW SEALED', 'price': 339.04, 'shipping': 0, 'time-left': '3d 4h left', 'time-end': '(Sun, 12:25 p.m.)', 'bid-count': 14, 'reviews-count': 0, 'url': 'https://www.Ebay.ca/itm/...'}
+```
